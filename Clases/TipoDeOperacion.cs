242c77cdd078017f36477c634ec68cf12bb518fb
Karen Cerrone <kacerrone@gmail.com>
2020-10-23 20:34:32 -0300

    Clases

---
+++ Clases/TipoDeOperacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Billetera.Models
+{
+    public class TipoDeOperacion
+    {
+        private int idTipoDeOperacion;
+        private string nombre;
+        private string descripcion;
+    }
+}
