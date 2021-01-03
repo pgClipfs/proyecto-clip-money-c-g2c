d0ec26e23e62adc2e6905b549c1eef268fc8a9e9
martincontrera <nc3020@gmail.com>
2021-01-03 17:07:00 -0300

    Make backend code for deposit operation.

---
+++ ClipMoney/Controllers/OperacionController.cs
@@ -0,0 +1,58 @@
+﻿using ClipMoney.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ClipMoney.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/operacion")]
+    public class OperacionController : ApiController
+    {
+        // GET: api/Operation
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Operacion/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Operation
+        [AllowAnonymous]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public void Post([FromBody] ParametroParaDeposito parametroParaDeposito)
+        {
+            Deposito deposito = new Deposito();
+            deposito.Monto = parametroParaDeposito.Monto;
+            deposito.IdCuenta = parametroParaDeposito.IdCuenta;
+            deposito.Depositar(parametroParaDeposito.Monto, parametroParaDeposito.IdCuenta);
+            //retornar id de la operacion?
+        }
+        //public void Post([FromBody] decimal monto, int id)
+        //{
+        //    Deposito deposito = new Deposito();
+        //    deposito.Monto = monto;
+        //    deposito.IdCuenta = id;
+        //    deposito.Depositar(monto, id);
+        //    //retornar id de la operacion?
+        //}
+
+        // PUT: api/Operation/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Operation/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ClipMoney/Models/OperacionBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClipMoney.Models
+{
+    public class OperacionBase
+    {
+        private int idOperacion;
+        private DateTime fechaDeOperacion;
+        private decimal monto;
+        private Nullable<long> cvuCbuDestino;
+        private int tipoDeOperacion;
+        private long idCuenta;
+
+        public OperacionBase()
+        {
+
+        }
+
+        public OperacionBase(int idOperacion, DateTime fechaDeOperacion, decimal monto, Nullable<long> cvuCbuDestino, int tipoDeOperacion, long idCuenta)
+        {
+            this.idOperacion = idOperacion;
+            this.fechaDeOperacion = fechaDeOperacion;
+            this.monto = monto;
+            this.cvuCbuDestino = cvuCbuDestino;
+            this.tipoDeOperacion = tipoDeOperacion;
+            this.idCuenta = idCuenta;
+        }
+
+        public int IdOperacion { get => idOperacion; set => idOperacion = value; }
+        public DateTime FechaDeOperacion { get => fechaDeOperacion; set => fechaDeOperacion = value; }
+        public decimal Monto { get => monto; set => monto = value; }
+        public int TipoDeOperacion { get => tipoDeOperacion; set => tipoDeOperacion = value; }
+        public long IdCuenta { get => idCuenta; set => idCuenta = value; }
+        public Nullable<long> CvuCbuDestino { get => cvuCbuDestino; set => cvuCbuDestino = value; }
+
+        //public abstract void Operar(decimal monto, int idCuenta); comentado porque las clases derivadas toman distintos parámetros
+    }
+
+}
