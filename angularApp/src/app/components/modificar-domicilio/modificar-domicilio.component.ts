e01474a8a3f7df1276d8063eeceb46a53b91f39b
Jonatan Torre Ledezma <jonatantorre05@gmail.com>
2021-01-05 23:38:55 -0300

    vistas perfil y modificar datos

---
+++ angularApp/src/app/components/modificar-domicilio/modificar-domicilio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-domicilio',
+  templateUrl: './modificar-domicilio.component.html',
+  styleUrls: ['./modificar-domicilio.component.scss']
+})
+export class ModificarDomicilioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
