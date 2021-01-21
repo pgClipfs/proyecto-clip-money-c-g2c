7d226f1db656836050b581307586a3929445f5ec
Karen Cerrone <kacerrone@gmail.com>
2021-01-21 14:08:03 -0300

    BD Actual con procedimientos agregados y cambios en el controller de Giros

--- ClipMoney/Controllers/GiroController.cs
+++ ClipMoney/Controllers/GiroController.cs
@@ -25,17 +25,20 @@ namespace ClipMoney.Controllers
         }
 
         // POST: api/Giro
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Giro/1
         [AllowAnonymous]
         [EnableCors(origins: "*", headers: "*", methods: "*")]
-        public string Put([FromBody] Operacion operacion)
+        public string Post([FromBody] Operacion operacion)
         {
             GestorOperaciones gestorOperaciones = new GestorOperaciones();
             return gestorOperaciones.RealizarGiroAlDescubierto(operacion.Monto, operacion.IdCuenta);
+        }
+
+        // PUT: api/Giro/1
+        
+        public string Put([FromBody] string value)
+        {
+            return value;
+            
         }
 
         // DELETE: api/Giro/5
